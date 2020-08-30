769f9a99b9de0791162cd4b99bbbec2b8b125682
rfj1986 <rfjiq1986@gmail.com>
2020-08-30 09:11:46 -0500

    Login normal sección 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -2,10 +2,13 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const _ = require("underscore");
 const Usuario = require("../models/usuario");
-
+const {
+    verificaToken,
+    verificaAdmin_Role,
+} = require("../middlewares/autenticacion");
 const app = express();
 
-app.get("/usuario", function(req, res) {
+app.get("/usuario", verificaToken, (req, res) => {
     let desde = req.query.desde || 0;
     desde = Number(desde);
 
@@ -23,7 +26,7 @@ app.get("/usuario", function(req, res) {
                 });
             }
 
-            Usuario.count({ estado: true }, (err, conteo) => {
+            Usuario.countDocuments({ estado: true }, (err, conteo) => {
                 res.json({
                     ok: true,
                     usuarios,
@@ -33,7 +36,7 @@ app.get("/usuario", function(req, res) {
         });
 });
 
-app.post("/usuario", function(req, res) {
+app.post("/usuario", [verificaToken, verificaAdmin_Role], function(req, res) {
     let body = req.body;
 
     let usuario = new Usuario({
@@ -55,7 +58,10 @@ app.post("/usuario", function(req, res) {
     });
 });
 
-app.put("/usuario/:id", function(req, res) {
+app.put("/usuario/:id", [verificaToken, verificaAdmin_Role], function(
+    req,
+    res
+) {
     let id = req.params.id;
     let body = _.pick(req.body, ["nombre", "email", "img", "role", "estado"]);
 
@@ -78,7 +84,10 @@ app.put("/usuario/:id", function(req, res) {
     );
 });
 
-app.delete("/usuario/:id", function(req, res) {
+app.delete("/usuario/:id", [verificaToken, verificaAdmin_Role], function(
+    req,
+    res
+) {
     let id = req.params.id;
 
     // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 require("./config/config");
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-
 const app = express();
+const bodyParser = require("body-parser");
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,7 +10,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(require("./routes/usuario"));
+// Configuracion global de rutas
+app.use(require("./routes/index"));
 
 mongoose.connect(
     process.env.URLDB, {
