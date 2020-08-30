a7415acf9f2b46759a2f78b3b13f229540df9cba
rfj1986 <rfjiq1986@gmail.com>
2020-08-29 20:51:17 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,6 +17,6 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
     urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB = "mongodb+srv://strider:1vvNbCQ2RbNqaQ31@sandbox.a6kd9.mongodb.net/cafe"
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
